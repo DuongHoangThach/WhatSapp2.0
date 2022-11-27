e29ef1cf364bdd12a70234e4360135dde0409ea4
DuongHoangThach <boduong95@gmail.com>
2022-11-27 15:57:19 +0700

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Loading from '../components/Loading';
+import { auth, db } from '../config/firebase';
+import '../styles/globals.css';
+import Login from './login';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [loggedInUser, loading, _error] = useAuthState(auth);
+
+  useEffect(() => {
+    const setUserInDb = async () => {
+      try {
+        await setDoc(
+          doc(db, 'users', loggedInUser?.email as string),
+          {
+            email: loggedInUser?.email,
+            lastSeen: serverTimestamp(),
+            photoURL: loggedInUser?.photoURL,
+          },
+          { merge: true } // just update what is changed
+        );
+      } catch (error) {
+        console.log('ERROR SETTING USER INFO IN DB', error);
+      }
+    };
+
+    if (loggedInUser) {
+      setUserInDb();
+    }
+  }, [loggedInUser]);
+
+  if (loading) return <Loading />;
+
+  if (!loggedInUser) return <Login />;
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
